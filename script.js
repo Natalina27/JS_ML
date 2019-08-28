d62b64a6c6f32ccddcefe31de083236c4c68e512
Natalya <natalyamyunster@yahoo.com>
2019-08-28 16:55:50 +0200

    Functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Variables
 document.write('Hello javascript!');
 document.write('Hello second');
 
@@ -7,3 +8,24 @@ let basicValue = 12345;
 let someText = 'Hello world';
 let gameOver = true;
 document.write(someText);
+
+//Functions
+
+//function declaration
+function showName(name, surname) {
+   return name + ' ' + surname;
+}
+
+//function invocation
+showName('Ivan', 'Ivanov');
+showName('Vova', 'Vovovich');
+
+let ivanName = 'Ivan';
+let ivanSurname = 'Ivanov';
+
+//function expression
+
+let ivanFullName = showName(ivanName, ivanSurname);
+console.log(ivanFullName);
+
+
