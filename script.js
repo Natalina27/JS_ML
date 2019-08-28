a24c4c21e4e57bed45b2f25528bf28bb2ce57831
Natalya <natalyamyunster@yahoo.com>
2019-08-28 14:40:51 +0200

    Variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.write('Hello javascript!')
+document.write('Hello javascript!');
+document.write('Hello second');
+
+let day;
+day = 1;
+let basicValue = 12345;
+let someText = 'Hello world';
+let gameOver = true;
+document.write(someText);
